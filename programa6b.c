0926b032ed0168a0b2fa3943e3d0efe1658cd01e
Pabvalb <pabvalb02@gmail.com>
2026-04-16 14:24:35 -0600

    Create programa6b.c

---
+++ programa6b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () 
+{
+    int lista[5] = {10, 8, 5, 8, 7};
+    int *ap = lista; 
+    int indice = 0;
+  
+    printf("\tLista\n");
+    while (indice < 5) 
+    {
+          printf("\nCalificación del alumno %d es %d", indice+1, *(ap+indice));
+          indice++; 
+    }
+    printf("\n");
+    return 0; 
+}
