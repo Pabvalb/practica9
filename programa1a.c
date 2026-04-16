424dfda7fa587c8c075f1b3a02a1ee71ba92c808
Pabvalb <pabvalb02@gmail.com>
2026-04-16 13:37:34 -0600

    Update programa1a.c

--- programa1a.c
+++ programa1a.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main () 
 {
-  int lista[5] = {10, 8, 5, 8, 7}; // Se declara e inicializa el arreglo unidimensional 
+  int lista[5] = {10, 8, 5, 8, 7}; 
   int indice = 0;
   printf("\tLista\n");
-  while (indice < 5 ) // Acceso a cada elemento del arreglo unidimensional usando while 
+  while (indice < 5 ) 
   {
         printf("\nCalificación del alumno %d es %d", indice+1, lista[indice]);
-        indice += 1; // Sentencia análoga a indice = indice + 1; 
+        indice += 1;  
   }
   printf("\n");
   return 0; 
